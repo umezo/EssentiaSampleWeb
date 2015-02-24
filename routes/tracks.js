a07c7152570b9996911b727d0dca436ddd8925b3
umezo <umezo100@gmail.com>
2015-02-24 19:49:33 +0900

    do not download if exists

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,6 +1,10 @@
+var fs = require('fs');
 var doDownload = require('../lib/do_download');
+
 var SC = require('node-soundcloud');
 SC.init(require(__dirname+'/../conf/soundcloud.json'));
+
+var SOUNDS_DIR = __dirname+'/../sounds';
 
 var router = require('express').Router();
 router.get('/', function(req, res, next) {
@@ -24,18 +28,41 @@ router.get('/:id', function(req, res, next) {
 router.get('/:id/beats', function(req, res, next) {
   var trackId = req.params.id;
 
+  var localTrack = new LocalTrack(trackId);
+
+  if (localTrack.exists()) {
+    res.render('tracks.html',{tracks:[]});
+    return;
+  }
+
   SC.get('/tracks/' + trackId, function(err, track) {
     if ( err ) { throw err; }
     var streamMetaPath = track.stream_url.replace(/https?:\/\/[^\/]+\//,'/');
 
     SC.get(streamMetaPath, function(err, stream){
       var file = stream.location.split('?')[0].split('/').pop();
-      doDownload(stream.location,__dirname+'/../sounds/'+trackId, file,function(err){
+      doDownload(stream.location, localTrack.getDir(), file,function(err){
         res.render('tracks.html',{tracks:[]});
       });
     });
   });
 });
+
+function LocalTrack(trackId){
+  this.id = trackId;
+}
+LocalTrack.prototype.getDir = function(){
+  return SOUNDS_DIR+'/'+this.id;
+};
+LocalTrack.prototype.exists = function(){
+  try {
+    fs.statSync(this.getDir());
+    return true;
+  } catch(e) {
+    return false;
+  }
+};
+
 
 
 
