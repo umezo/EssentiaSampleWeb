5d2ccbfb06a1fe220097f9553f190f0ee8f1f1c1
umezo <umezo100@gmail.com>
2015-03-04 17:49:12 +0900

    refactor

--- app.js
+++ app.js
@@ -8,20 +8,26 @@ var bodyParser = require('body-parser');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
+app.engine('html', require('./lib/template-engine'));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+//ミドルウェア系
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 
+//ディレクトリとか
+app.use(favicon(__dirname + '/public/img/tv.gif'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+
+
+//ルーティング
 app.use('/', require('./routes/index'));
 app.use('/tracks', require('./routes/tracks'));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -30,10 +36,21 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-var templateEngine = require('./lib/template_engine');
-app.engine('html', templateEngine);
 
-// error handlers
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // development error handler
 // will print stacktrace
--- routes/tracks.js
+++ routes/tracks.js
@@ -11,77 +11,100 @@ var SOUNDS_DIR = __dirname+'/../'+config.get('dir.sounds');
 var SC = require('node-soundcloud');
 SC.init(config.get('soundcloud'));
 
+
+function TrackController(){}
+
+_.assign(TrackController.prototype,{
+  /**
+   * 適当にSoundCloudから曲を取ってきて出す
+   * querystringはtagsだけ対応
+   * 絞り込みはハードコーディング、短すぎても長すぎても困るので
+   */
+  index: function(req,res,next){
+
+    //soundcloud用のクエリ
+    var query = querystring.stringify({
+      'bpm[from]': 120,
+      'bpm[to]': 150,
+      'duration[from]': 10 * 1000,
+      'duration[to]': 90 * 1000,
+      tags:req.query.tags||'house',
+      filter:'public',
+      limit:20
+    });
+
+    //sound cloud API call
+    SC.get('/tracks?' + query, function(err, tracks) {
+      if ( err ) { next(err); }
+
+      //あとで曲をダウンロードするのでstream可能なやつだけに絞る limit=5
+      tracks = _.filter(tracks,function(e){return e.streamable;}).slice(0,5);
+      res.render('tracks.html',{tracks:tracks});
+    });
+  },
+
+  /**
+   * params.idで指定されたidのsound cloud trackを表示する
+   */
+  track: function(req,res,next) {
+    SC.get('/tracks/' + req.params.id , function(err, track) {
+      if ( err ) { next(new Error(JSON.stringify(err))); }
+      res.render('tracks.html',{tracks:[track]});
+    });
+  },
+
+  /**
+   * params.idで指定されたidのビート情報を返す
+   */
+  beats: function(req,res,next) {
+    var trackId = req.params.id;
+
+
+    var soundCloudTrackStore = new SoundCloudTrackStore(trackId);
+    soundCloudTrackStore.download(SOUNDS_DIR+'/'+trackId,function(err,filepath){
+      if (err) {throw err;}
+
+      var localTrack = new LocalTrack(filepath);
+
+      localTrack.on('initialize',function(err,track){
+        res.header('Content-Type','application/json');
+        res.send(JSON.stringify(localTrack.beats()));
+      }).on('error',function(err){
+        throw err;
+      });
+
+      localTrack.initialize();
+    });
+  },
+
+  /**
+   * params.idで指定されたidのビートクリック音が合成された曲本体を返す
+   */
+  stream: function(req,res,next) {
+    var trackId = req.params.id;
+    var soundCloudTrackStore = new SoundCloudTrackStore(trackId);
+    soundCloudTrackStore.download(SOUNDS_DIR+'/'+trackId,function(err,filepath){
+      if (err) { throw err; }
+
+      var localTrack = new LocalTrack(filepath);
+
+      localTrack.on('initialize',function(err,track){
+        // これハードコーディングしてんのダメだな
+        res.redirect('/sounds/'+trackId+'/beats.mp3');
+      }).on('error',function(err){
+        res.send(JSON.stringify(err));
+      });
+
+      localTrack.initialize();
+    });
+  }
+});
+
+var controller = new TrackController();
 var router = require('express').Router();
-router.get('/', function(req, res, next) {
-  var query = querystring.stringify({
-    'bpm[from]': 120,
-    'bpm[to]': 150,
-    'duration[from]': 10 * 1000,
-    'duration[to]': 90 * 1000,
-    tags:req.query.tags||'house',
-    filter:'public',
-    limit:20
-  });
-  SC.get('/tracks?' + query, function(err, tracks) {
-    if ( err ) {
-      throw err;
-    }
-    res.render('tracks.html',{
-      tracks:_.filter(tracks,function(e){return e.streamable;}).slice(0,5)
-    });
-  });
-});
-
-router.get('/:id', function(req, res, next) {
-
-  SC.get('/tracks/' + req.params.id , function(err, track) {
-    if ( err ) {
-      throw err;
-    }
-    res.render('tracks.html',{tracks:[track]});
-  });
-});
-
-router.get('/:id/beats', function(req, res, next) {
-  var trackId = req.params.id;
-  var soundCloudTrackStore = new SoundCloudTrackStore(trackId);
-  soundCloudTrackStore.download(SOUNDS_DIR+'/'+trackId,function(err,filepath){
-    if (err) {
-      throw JSON.stringify(err);
-    }
-
-    var localTrack = new LocalTrack(filepath);
-
-    localTrack.on('initialize',function(err,track){
-      res.header('Content-Type','application/json');
-      res.send(JSON.stringify(localTrack.beats()));
-    }).on('error',function(err){
-      res.send(JSON.stringify(err));
-    });
-
-    localTrack.initialize();
-  });
-});
-
-router.get('/:id/beats/stream', function(req, res, next) {
-  var trackId = req.params.id;
-  var soundCloudTrackStore = new SoundCloudTrackStore(trackId);
-  soundCloudTrackStore.download(SOUNDS_DIR+'/'+trackId,function(err,filepath){
-    if (err) {
-      console.log(err);
-      throw err;
-    }
-
-    var localTrack = new LocalTrack(filepath);
-
-    localTrack.on('initialize',function(err,track){
-      res.redirect('/sounds/'+trackId+'/beats.mp3');
-    }).on('error',function(err){
-      res.send(JSON.stringify(err));
-    });
-
-    localTrack.initialize();
-  });
-});
+router.get('/', controller.index);
+router.get('/:id', controller.track);
+router.get('/:id/beats', controller.beats);
+router.get('/:id/beats/stream', controller.stream);
 
 module.exports = router;
